6557aaa71071919237b3fdf7e397fa900b8787d5
asuraxxd <ahiram.rivas@institutvidreres.cat>
2025-11-16 18:22:01 +0100

    Ejercicio 1

---
+++ JSFuncions_Ahiram_Rivas.js
@@ -0,0 +1,7 @@
+//1.- Passa el teu nom i cognoms com a paràmetres i els mostri per consola.
+
+function nomCognom(nom,cognom){
+    console.log("nom: "+nom);
+    console.log("cognom: "+cognom);
+}
+nomCognom("ahiram","rivas");
