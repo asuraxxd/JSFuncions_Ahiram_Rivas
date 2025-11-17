69c9352061579e4f21b0f08d61c6fb56dfbf9be0
asuraxxd <ahiram.rivas@institutvidreres.cat>
2025-11-17 18:30:27 +0100

    Ejercicio 4

--- JSFuncions_Ahiram_Rivas.js
+++ JSFuncions_Ahiram_Rivas.js
@@ -22,4 +22,15 @@ function fechaActual() {
     const fecha = new Date();
     console.log("La data actual es: " + fecha)
 }
+
 fechaActual()
+
+//4.- Declara una variable dins d’una funció i mostra el seu valor fora d’aquesta. Què passa? Perquè?
+
+function variableDentro() {
+    let variable = "hola"
+    console.log("Dins de la funcio:", variable)
+}
+
+variableDentro();
+console.log("fora de la funcio:", variable)
